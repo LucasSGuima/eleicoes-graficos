37ade0bfee3632ee04211e855a0ffeb8358497b3
LucasSGuima <contato@lucassilvaguimaraes.com.br>
2024-10-04 19:42:16 -0300

    feat: Adicionando animação

--- src/views/Vote.vue
+++ src/views/Vote.vue
@@ -1,5 +1,5 @@
 <template>
-  <main id="app" class="container flex items-center justify-center w-full h-screen px-4 mx-auto">
+  <main id="app" class="container flex items-center justify-center w-full h-auto px-4 py-10 mx-auto sm:py-0 sm:h-screen">
     
     <celebration v-if="celebration">
       <div v-for="(candidate, index) in candidates" :key="index">
@@ -30,95 +30,137 @@
       </div>
     </celebration>
 
-    <voteSection v-else="celebration">
-      <section>
-        <p class="text-lg font-normal text-gray-400">
-          última atualização em {{ updateTime }} (horário de brasília)
-        </p>
-
-        <div class="flex items-end justify-between mt-5">
-          <p class="mb-5 text-5xl font-bold text-gray-900">{{ location }}</p>
-          <p class="mb-5 text-2xl text-gray-900">{{ percentApurado }}%</p>
-        </div>
-
-        <div class="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
-          <div class="h-1 bg-blue-600 rounded-full dark:bg-blue-500" :style="{ width: `${percentApurado}%` }"></div>
-        </div>
-
-        <p class="text-lg text-gray-600">seções apuradas: {{ secoesApuradas }}</p>
-      </section>
-
-      <section class="mt-20">
-        <p class="text-2xl font-bold text-gray-900">Prefeito</p>
-
-        <!-- Renderiza a lista de candidatos -->
-        <div v-for="(candidate, index) in candidates" :key="index">
-
-          <candidate class="flex items-center mt-5">
-            <img class="w-20 h-20 rounded-full" :src="candidate.image" alt="image description" />
-
-            <div class="flex-grow ml-4">
-              <div class="flex justify-between">
-                <div class="flex flex-col items-start">
-                  <span class="text-lg font-bold">{{ candidate.name }}</span>
-
-                  <group>
-                    <span class="mb-1 text-sm font-lg">{{ candidate.party }}</span>
-                  </group>
+    <voteSection v-else="celebration" class="w-full">
+      <blockSection class="flex flex-col items-center justify-center w-full">
+        <section>
+          <p class="text-lg font-normal text-gray-400">
+            última atualização em {{ updateTime }} (horário de brasília)
+          </p>
+  
+          <div class="flex items-end justify-between mt-5">
+            <p class="mb-5 text-5xl font-bold text-gray-900">{{ location }}</p>
+            <p class="mb-5 text-2xl text-gray-900"><span ref="percentApuradoRef">{{ percentApurado }}</span>%</p>
+          </div>
+  
+          <div class="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+            <div ref="progressBar" class="h-1 bg-blue-600 rounded-full dark:bg-blue-500" :style="{ width: `${percentApurado}%` }"></div>
+          </div>
+  
+          <p class="text-lg text-gray-600">seções apuradas: <span ref="secoesApuradasRef">{{ secoesApuradas }}</span></p>
+        </section>
+  
+        <section class="mt-20">
+          <p class="text-2xl font-bold text-gray-900">Prefeito</p>
+  
+          <!-- Renderiza a lista de candidatos -->
+          <div v-for="(candidate, index) in candidates" :key="index">
+  
+            <candidate class="flex items-center mt-5">
+              <img class="w-20 h-20 rounded-full" :src="candidate.image" alt="image description" />
+  
+              <div class="flex-grow ml-4">
+                <div class="flex justify-between">
+                  <div class="flex flex-col items-start">
+                    <span class="text-lg font-bold">{{ candidate.name }}</span>
+  
+                    <group>
+                      <span class="mb-1 text-sm font-lg">{{ candidate.party }}</span>
+                    </group>
+                  </div>
+  
+                  <div class="text-right">
+                    <div class="mb-1 text-lg font-bold">
+                      <span id="'percentCadidate' + index" :ref="'percentRef' + index">{{ candidate.percent }}</span>%
+                    </div>
+                    <div class="mb-1 text-sm font-medium text-gray-400">
+                      <span :ref="'votesRef' + index">{{ candidate.votes }}</span> votos
+                    </div>
+                  </div>
                 </div>
-
-                <div class="text-right">
-                  <div class="mb-1 text-lg font-bold">{{ candidate.percent }}%</div>
-                  <div class="mb-1 text-sm font-medium text-gray-400">{{ candidate.votes }} votos</div>
+  
+                <div v-if="candidate.meeeeuPrefeito" class="w-full h-3 bg-gray-200 rounded-full dark:bg-gray-700">
+                  <div :ref="'percentRefBar' + index" class="h-3 bg-blue-600 rounded-full dark:bg-blue-500" :style="{ width: `${candidate.percent}%` }">
+                  </div>
+                </div>
+                <div v-else class="w-full h-1 bg-gray-200 rounded-full dark:bg-gray-700">
+                  <div :ref="'percentRefBar' + index" class="h-1 bg-blue-600 rounded-full dark:bg-blue-500" :style="{ width: `${candidate.percent}%` }">
+                  </div>
                 </div>
               </div>
+            </candidate>
+  
+          </div>
+        </section>
+      </blockSection>
 
-              <div v-if="candidate.meeeeuPrefeito" class="w-full h-3 bg-gray-200 rounded-full dark:bg-gray-700">
-                <div class="h-3 bg-blue-600 rounded-full dark:bg-blue-500" :style="{ width: `${candidate.percent}%` }">
-                </div>
-              </div>
-              <div v-else class="w-full h-1 bg-gray-200 rounded-full dark:bg-gray-700">
-                <div class="h-1 bg-blue-600 rounded-full dark:bg-blue-500" :style="{ width: `${candidate.percent}%` }">
-                </div>
-              </div>
+      <blockSection class="flex flex-col items-center justify-center w-full">
+        <section class="grid grid-cols-1 gap-4 mt-20 text-center md:w-3/5 sm:grid-cols-2 lg:grid-cols-5">
+      
+          <div class="flex flex-col items-center p-4 text-center bg-white border border-gray-200 rounded-lg shadow">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <span class="font-bold">VÁLIDOS</span>
+            <div class="flex">
+              <span ref="validosRef">{{ validos }}</span>
+              (<span ref="validosPercentRef">{{ validosPercent }}</span>%)
             </div>
-          </candidate>
+          </div>
+      
+          <div class="flex flex-col items-center p-4 text-center bg-white border border-gray-200 rounded-lg shadow">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <span class="font-bold">BRANCOS</span>
+            <div class="flex">
+              <span ref="brancosRef">{{ brancos }}</span>
+              (<span ref="brancosPercentRef">{{ brancosPercent }}</span>%)
+            </div>
+          </div>
+      
+          <div class="flex flex-col items-center justify-center p-4 text-center bg-white border border-gray-200 rounded-lg shadow">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <span class="font-bold">NULOS</span>
+            <div class="flex">
+              <span ref="nulosRef">{{ nulos }}</span>
+              (<span ref="nulosPercentRef">{{ nulosPercent }}</span>%)
+            </div>
+          </div>
+      
+          <div class="flex flex-col items-center p-4 text-center bg-white border border-gray-200 rounded-lg shadow lg:items-end">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+              </svg>
+            </span>
+            <span class="font-bold">ABSTENÇÕES</span>
+            <div class="flex">
+              <span ref="abstencoesRef">{{ abstencoes }}</span>
+              (<span ref="abstencoesPercentRef">{{ abstencoesPercent }}</span>%)
+            </div>
+          </div>
 
-        </div>
-      </section>
-
-      <section class="grid grid-cols-5 gap-4 mt-20 text-center">
-        <div class="flex flex-col items-start">
-          <span class="font-bold">TOTAL</span>
-          <span>{{ total }}</span>
-        </div>
-
-        <div class="flex flex-col items-center">
-          <span class="font-bold">VÁLIDOS</span>
-          <span>{{ validos }} ({{ validosPercent }}%)</span>
-        </div>
-
-        <div class="flex flex-col items-center">
-          <span class="font-bold">BRANCOS</span>
-          <span>{{ brancos }} ({{ brancosPercent }}%)</span>
-        </div>
-
-        <div class="flex flex-col items-center">
-          <span class="font-bold">NULOS</span>
-          <span>{{ nulos }} ({{ nulosPercent }}%)</span>
-        </div>
-
-        <div class="flex flex-col items-end">
-          <span class="font-bold">ABSTENÇÕES</span>
-          <span>{{ abstencoes }} ({{ abstencoesPercent }}%)</span>
-        </div>
-      </section>
+          <div class="flex flex-col items-center col-span-2 p-4 bg-white border border-gray-200 rounded-lg shadow">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+            </svg>
+            <span class="font-bold">TOTAL</span>
+            <div class="flex">
+              <span ref="totalRef">{{ total }}</span>
+            </div>
+          </div>
+        </section>
+      </blockSection>
+      
     </voteSection>
 
   </main>
 </template>
 
 <script>
+  import anime from 'animejs/lib/anime.es.js'; // Importa o anime.js
   import WebSocketService from '@/services/websocket';
   import confetti from 'canvas-confetti'; // Importa o canvas-confetti
 
@@ -161,6 +203,35 @@
         ws.connect();
 
         ws.onMessage((data) => {
+
+          this.candidates = data.candidates.map((candidate, index) => {
+            // Anima o percentual de votos
+            this.animateNumber(this.candidates[index]?.percent || 0, candidate.percent, `percentRef${index}`, true);
+            // Anima o número de votos
+            this.animateNumber(this.candidates[index]?.votes || 0, candidate.votes, `votesRef${index}`);
+            
+            // Anima a barra de progresso individual do candidato
+            this.animateProgressBar(candidate.percent, `percentRefBar${index}`);
+
+            return candidate; // Retorna o candidato atualizado
+          });
+
+          // Atualiza o estado com os dados recebidos
+          this.animateNumber(this.percentApurado, data.percentApurado, 'percentApuradoRef');
+          this.animateNumber(this.secoesApuradas, data.secoesApuradas, 'secoesApuradasRef');
+          this.animateNumber(this.candidates, data.candidates, 'candidatesRef');
+          this.animateNumber(this.total, data.total, 'totalRef');
+          this.animateNumber(this.validos, data.validos, 'validosRef');
+          this.animateNumber(this.validosPercent, data.validosPercent, 'validosPercentRef');
+          this.animateNumber(this.brancos, data.brancos, 'brancosRef');
+          this.animateNumber(this.brancosPercent, data.brancosPercent, 'brancosPercentRef');
+          this.animateNumber(this.nulos, data.nulos, 'nulosRef');
+          this.animateNumber(this.nulosPercent, data.nulosPercent, 'nulosPercentRef');
+          this.animateNumber(this.abstencoes, data.abstencoes, 'abstencoesRef');
+          this.animateNumber(this.abstencoesPercent, data.abstencoesPercent, 'abstencoesPercentRef');
+          
+          this.animateProgressBar(data.percentApurado, 'progressBar'); // Anima a barra de progresso
+
           // Atualiza o estado com os dados recebidos
           this.updateTime = data.updateTime;
           this.percentApurado = data.percentApurado;
@@ -178,6 +249,28 @@
 
           // Verifica se é hora de celebrar
           this.celebration = data.celebration;
+
+        });
+      },
+
+      animateNumber(oldValue, newValue, refName, decimal = false) {
+        anime({
+          targets: this.$refs[refName],
+          innerHTML: [oldValue, newValue],
+          round: 10, // Arredonda o valor para inteiros
+          easing: 'easeInOutQuad',
+          duration: 4000,
+          translateX: [0, 0]
+        });
+      },
+
+      animateProgressBar(newPercent, refName) {
+        anime({
+          targets: this.$refs[refName],
+          width: `${newPercent}%`,
+          easing: 'easeInOutQuad',
+          duration: 4000,
+          translateX: [0, 0]
         });
       },
 
